7fa0a41815732fa98b487e37a690e5e1975919f6
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-05 16:30:43 +0530

    add gradient animation to the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import React from "react";
+
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <div className="w-[100vw] h-[6vh] bg-gradient-to-r from-[rgba(225,14,14,0.81)] to-[rgba(225,14,14,0.0)] opacity-0 hover:opacity-100 transition-opacity duration-[2s]">
+      hiiiiii
+    </div>
+  );
 };
 
 export default App;
