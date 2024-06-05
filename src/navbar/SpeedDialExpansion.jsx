d533c98e07874ca3aa2ccf5d5b152f1a8b62dd93
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-06 01:33:24 +0530

    add incoming animation to the icons

--- src/navbar/SpeedDialExpansion.jsx
+++ src/navbar/SpeedDialExpansion.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import OtherHousesOutlinedIcon from '@mui/icons-material/OtherHousesOutlined';
 import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
 import CollectionsOutlinedIcon from '@mui/icons-material/CollectionsOutlined';
@@ -6,15 +6,21 @@ import AddIcCallOutlinedIcon from '@mui/icons-material/AddIcCallOutlined';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 
 function SpeedDialExpansion() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(true);
+  }, []);
+
   return (
-    <div className='absolute flex flex-row  space-x-[10vw] top-3 left-[10vw]'>
-      <OtherHousesOutlinedIcon/>
-      <HelpOutlineOutlinedIcon/>
-      <CollectionsOutlinedIcon/>
-      <InfoOutlinedIcon/>
-       <AddIcCallOutlinedIcon/>
+    <div className={`absolute flex flex-row space-x-[10vw] top-3 left-[10vw] ${isOpen ? 'open' : ''}`}>
+      <OtherHousesOutlinedIcon className='hover:cursor-pointer'/>
+      <HelpOutlineOutlinedIcon className='hover:cursor-pointer'/>
+      <CollectionsOutlinedIcon className='hover:cursor-pointer'/>
+      <InfoOutlinedIcon className='hover:cursor-pointer'/>
+      <AddIcCallOutlinedIcon className='hover:cursor-pointer'/>
     </div>
-  )
+  );
 }
 
-export default SpeedDialExpansion
+export default SpeedDialExpansion;
