d6913a5f20efe4e6645393f8d1d081f8f02e1be0
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-05 16:33:23 +0530

    add navbar component

---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="w-[100vw] h-[6vh] bg-gradient-to-r from-[rgba(225,14,14,0.81)] to-[rgba(225,14,14,0.0)] opacity-0 hover:opacity-100 transition-opacity duration-[2s]">
+    hiiiiii
+  </div>
+  )
+}
+
+export default Navbar
