e39b7adadd25a1f199dd280cb905cbb749b2d6ba
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-09 00:17:29 +0530

    make speed dial responsive

--- src/navbar/SpeedDialExpansion.jsx
+++ src/navbar/SpeedDialExpansion.jsx
@@ -4,6 +4,7 @@ import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import CollectionsOutlinedIcon from "@mui/icons-material/CollectionsOutlined";
 import AddIcCallOutlinedIcon from "@mui/icons-material/AddIcCallOutlined";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import { Box } from "@mui/material";
 
 function SpeedDialExpansion({ isClicked }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,115 +19,51 @@ function SpeedDialExpansion({ isClicked }) {
   };
 
   return (
-    <div className="w-screen absolute  pt-[1vw] flex justify-evenly items-center h-12 sm:h-14 md:h-16 lg:h-20">
-      <div
-        className='text-white flex justify-center items-end gap-3 sm:gap-5 md:gap-6 lg:gap-10 ml-16 sm:ml-24 md:ml-24 lg:ml-40 '
-      >
-        <a
-          value="1"
-          onClick={() => clickHandler(1)}
-          href="#home"
-          className={`flex flex-col items-center hover:scale-125 animate__animated ${
-            isClicked ? "animate__fadeInRight" : "animate__fadeOutRight"
-          } animate__faster hover:text-violet-500 transition-transform transform hover:scale-110`}
-        >
-          <OtherHousesOutlinedIcon
-            className={`hover:cursor-pointer animate__animated  ${
-              clickedIcon === 1 ? "text-violet-800 animate__bounceIn" : ""
-            }`}
-          />
-          <div
-            className={`animate__animated ${
-              clickedIcon === 1 ? "text-violet-800 animate__rubberBand" : ""
-            } text-xs sm:text-sm md:text-base lg:text-lg`}
+    <div className="w-screen absolute pt-[1vw] flex justify-evenly items-center h-12 sm:h-14 md:h-16 lg:h-20">
+      <div className="text-white flex justify-center items-end gap-3 sm:gap-5 md:gap-6 lg:gap-10 ml-16 sm:ml-24 md:ml-24 lg:ml-40">
+        {[
+          { icon: OtherHousesOutlinedIcon, label: "Home", href: "#home", value: 1 },
+          { icon: HelpOutlineOutlinedIcon, label: "What We Do", href: "#whatWeDo", value: 2 },
+          { icon: CollectionsOutlinedIcon, label: "Gallery", href: "#gallery", value: 3 },
+          { icon: InfoOutlinedIcon, label: "About Us", href: "#aboutUs", value: 4 },
+          { icon: AddIcCallOutlinedIcon, label: "Contact", href: "#contact", value: 5 },
+        ].map(({ icon: Icon, label, href, value }) => (
+          <a
+            key={value}
+            onClick={() => clickHandler(value)}
+            href={href}
+            className={`flex flex-col items-center animate__animated ${
+              isClicked ? "animate__fadeInRight" : "animate__fadeOutRight"
+            } animate__faster hover:text-violet-500 transition-transform transform hover:scale-110`}
           >
-            Home
-          </div>
-        </a>
-        <a
-          value="2"
-          onClick={() => clickHandler(2)}
-          href="#whatWeDo"
-          className={`flex flex-col items-center animate__animated ${
-            isClicked ? "animate__fadeInRight" : "animate__fadeOutRight"
-          } animate__faster hover:text-violet-500 transition-transform transform hover:scale-110`}
-        >
-          <HelpOutlineOutlinedIcon
-            className={`hover:cursor-pointer animate__animated  ${
-              clickedIcon === 2 ? "text-violet-800 animate__bounceIn" : ""
-            }`}
-          />
-          <div
-            className={`animate__animated ${
-              clickedIcon === 2 ? "text-violet-800 animate__rubberBand" : ""
-            } text-xs sm:text-sm md:text-base lg:text-lg`}
-          >
-            What We Do
-          </div>
-        </a>
-        <a
-          value="3"
-          onClick={() => clickHandler(3)}
-          href="#gallery"
-          className={`flex flex-col items-center animate__animated ${
-            isClicked ? "animate__fadeInRight" : "animate__fadeOutRight"
-          } animate__faster hover:text-violet-500 transition-transform transform hover:scale-110`}
-        >
-          <CollectionsOutlinedIcon
-            className={`hover:cursor-pointer animate__animated  ${
-              clickedIcon === 3 ? "text-violet-800 animate__bounceIn" : ""
-            }`}
-          />
-          <div
-            className={`animate__animated ${
-              clickedIcon === 3 ? "text-violet-800 animate__rubberBand" : ""
-            } text-xs sm:text-sm md:text-base lg:text-lg`}
-          >
-            Gallery
-          </div>
-        </a>
-        <a
-          value="4"
-          onClick={() => clickHandler(4)}
-          href="#aboutUs"
-          className={`flex flex-col items-center animate__animated ${
-            isClicked ? "animate__fadeInRight" : "animate__fadeOutRight"
-          } animate__faster hover:text-violet-500 transition-transform transform hover:scale-110`}
-        >
-          <InfoOutlinedIcon
-            className={`hover:cursor-pointer animate__animated  ${
-              clickedIcon === 4 ? "text-violet-800 animate__bounceIn" : ""
-            }`}
-          />
-          <div
-            className={`animate__animated ${
-              clickedIcon === 4 ? "text-violet-800 animate__rubberBand" : ""
-            } text-xs sm:text-sm md:text-base lg:text-lg`}
-          >
-            About Us
-          </div>
-        </a>
-        <a
-          value="5"
-          onClick={() => clickHandler(5)}
-          href="#contact"
-          className={`flex flex-col items-center justify-center animate__animated ${
-            isClicked ? "animate__fadeInRight" : "animate__fadeOutRight"
-          } animate__faster hover:text-violet-500 transition-transform transform hover:scale-110`}
-        >
-          <AddIcCallOutlinedIcon
-            className={`hover:cursor-pointer animate__animated  ${
-              clickedIcon === 5 ? "text-violet-800 animate__bounceIn" : ""
-            }`}
-          />
-          <div
-            className={`animate__animated ${
-              clickedIcon === 5 ? "text-violet-800 animate__rubberBand" : ""
-            } text-xs sm:text-sm md:text-base lg:text-lg`}
-          >
-            Contact
-          </div>
-        </a>
+            <Box
+              component={Icon}
+              sx={{
+                fontSize: {
+                  xs: 'small', // Small size for extra small screens
+                  sm: 'medium', // Medium size for small screens
+                  md: 'large', // Large size for medium screens
+                  lg: 'x-large', // Extra large size for large screens
+                },
+                cursor: 'pointer',
+                color: clickedIcon === value ? 'rgb(107 33 168)' : 'inherit', // Tailwind color class for text-violet-800
+                '&:hover': {
+                  color: 'rgb(139 92 246)', // Tailwind color class for text-violet-500
+                },
+                ...(clickedIcon === value && {
+                  animation: 'bounceIn 1s',
+                }),
+              }}
+            />
+            <div
+              className={`animate__animated ${
+                clickedIcon === value ? "text-violet-800 animate__rubberBand" : ""
+              } text-xs sm:text-xs md:sm lg:text-base xl:text-lg`}
+            >
+              {label}
+            </div>
+          </a>
+        ))}
       </div>
     </div>
   );
