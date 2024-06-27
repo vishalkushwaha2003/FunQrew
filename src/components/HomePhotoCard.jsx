e1178ca648550e7185f06f663fabada00dbfb780
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-06-28 01:00:11 +0530

    change founder photo

--- src/components/HomePhotoCard.jsx
+++ src/components/HomePhotoCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import f4 from '../assets/homePhoto/founder4.png'
+import f4 from '../assets/homePhoto/founder3.png'
 
 function HomePhotoCard() {
   return (
     <div class="card  ">
-        <img src={f4}  alt="founder photo" className='animate__animated animate__backInRight'/>
+        <img src={f4}  alt="founder photo" className='animate__animated animate__backInRight rounded-[20px]'/>
   <div class="card__content">
     <p class="card__title">Ankit Srivastva</p>
     <p class="card__description"> Founder of FunQrew</p>
