10f09595a8ccd360908e9730db1b3c1fc4831925
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-07-30 19:02:29 +0530

    give padding to the top of the home page component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div
       id="home"
-      className={`h-full w-full grid grid-cols-12 grid-rows-12 overflow-hidden bg-gradient-to-br from-[rgba(104,35,162,0.30)] to-[rgba(104,35,162,0.91)] `}
+      className={`h-full w-full grid grid-cols-12 grid-rows-12 overflow-hidden bg-gradient-to-br from-[rgba(104,35,162,0.30)] to-[rgba(104,35,162,0.91)] sm:pt-0 pt-12`}
     >
       <div className="justify-center items-center col-span-12 row-span-6  sm:col-span-6 sm:row-span-12 flex sm:justify-end sm:items-center overflow-hidden p-10">
         <HomePhotoCard className="animate__animated animate__fadeInRightBig" />
